aefdb95f7019cb74d45ce99a933f47c555076718
Samir Joshi <samirjoshi@pop-os.localdomain>
2020-04-01 00:09:31 -0700

    implement basic website

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import './App.scss'
+import { LandingPage } from './components/LandingPage'
 
 function App() {
   return (
-    <div className="App">
-      <section className="hero has-background-white-ter is-fullheight">
-        <div className="hero-body">
-          <div className="tile is-ancestor is-vertical">
-            <div className="tile is-parent">
-              <div className="tile is-4 is-danger is-child front-page-tile">
-              </div>
-              <div className="tile is-4 is-danger is-child">
-                <h1 className="title has-text-primary">
-                  samir joshi 
-                </h1>
-              </div>
-              <div className="tile is-4 is-danger is-child front-page-tile">
-              </div>
-            </div>
-            <div className="tile is-parent">
-              <div className="tile is-4 is-danger is-child front-page-tile">
-                <div className='box tile-card'>
-                  Personal Projects
-                </div>
-              </div>
-              <div className="tile is-4 is-danger is-child front-page-tile">
-                <div className='box tile-card'>
-                  Resume
-                </div>
-              </div>
-              <div className="tile is-4 is-danger is-child front-page-tile">
-                <div className='box tile-card'>
-                  About Me
-                  Interests - Research papers/podcasts/ etc
-                  Social Media
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={LandingPage} />
+      </Switch>
+    </Router>
   );
 }
 
